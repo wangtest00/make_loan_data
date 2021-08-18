a85ae478c13b31aa89742b14f2ed56a56b75551a
wangtest00 <370558913@qq.com>
2021-08-17 21:09:08 -0500

    1.最新修改，解决jenkins

--- mexico/main.py
+++ mexico/main.py
@@ -1,12 +1,12 @@
 import string,requests,json,datetime
-from public.dataBase import *
-from mexico.gaishu import *
-from data.var_mex import *
-from mexico.mex_mgt import *
-from mexico.heads import *
+from make_loan_data.public.dataBase import *
+from make_loan_data.mexico.gaishu import *
+from make_loan_data.data.var_mex import *
+from make_loan_data.mexico.mex_mgt import *
+from make_loan_data.mexico.heads import *
 import io,sys
 #改编码方便jenkins运行
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="gb18030")
+#sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="gb18030")
 
 #注册,认证，提交多种信息申请贷款
 def first_login_apply(registNo):
@@ -25,6 +25,7 @@ def first_login_apply(registNo):
         DataBase(which_db).closeDB()
     else:
         bank_auth(custNo,headt)
+        pl_shenpi()
         update_appr_user_stat()
         DataBase(which_db).call_4_proc()
         approve(loan_no)  #分配审批人员并审批通过
@@ -40,28 +41,6 @@ def auto_test():
         registNo=str(random.randint(8000000000,9999999999)) #10位随机数作为手机号
         first_login_apply(registNo)
 
-#指定手机号，跑后续流程
-def bu_ding(registNo):
-    token=login_pwd(registNo)
-    headt=head_token(token)
-    sql="select CUST_NO from cu_cust_reg_dtl where REGIST_NO='"+registNo+"';"
-    custNo=DataBase(which_db).get_one(sql)
-    custNo=custNo[0]
-    loan_no=apply_loan(custNo,headt)
-    if loan_no is None:
-        DataBase(which_db).closeDB()
-    else:
-        bank_auth(custNo,headt)
-        update_appr_user_stat()
-        DataBase(which_db).call_4_proc()
-        approve(loan_no)  #分配审批人员并审批通过
-        insert_risk(loan_no)
-        w=withdraw(registNo,custNo,loan_no,headt)
-        if w==1:
-            gaishu(loan_no)
-        else:
-            pass
-        DataBase(which_db).closeDB()
 
 if __name__ == '__main__':
     auto_test()

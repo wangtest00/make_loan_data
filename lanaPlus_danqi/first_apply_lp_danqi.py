91669f49d73c2f9934c7cd2f1205edeb14e3fcf1
wangshuang <370558913@qq.com>
2022-04-24 14:57:25 +0800

    0424恢复优化

--- lanaPlus_danqi/first_apply_lp_danqi.py
+++ lanaPlus_danqi/first_apply_lp_danqi.py
@@ -3,67 +3,74 @@ from lanaPlus_danqi.gaishu_lp_danqi import *
 from lanaPlus_danqi.daiqian_lp_danqi import *
 from lanaPlus_danqi.mgt_lp_danqi import *
 
-#import io,sys
-#改编码方便jenkins运行
-#sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="gb18030")
 
-#注册,认证，提交多种信息申请贷款到达待审批状态
+# import io,sys
+# 改编码方便jenkins运行
+# sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="gb18030")
+
+# 注册,认证，提交多种信息申请贷款到达待审批状态
 def first_apply(registNo):
     update_pwd(registNo)
-    token=login_pwd(registNo)
-    headt=head_token(token)
-    custNo=auth_cert(registNo,headt)
-    auth_work(custNo,headt)
-    auth_review_contact(custNo,headt)
-    auth_app_grab_data(registNo,custNo,headt)
-    auth_contact(custNo,headt)
-    update_kyc_auth(registNo,custNo)
+    token = login_pwd(registNo)
+    headt = head_token(token)
+    custNo = auth_cert(registNo, headt)
+    auth_work(custNo, headt)
+    auth_review_contact(custNo, headt)
+    auth_app_grab_data(registNo, custNo, headt)
+    auth_contact(custNo, headt)
+    update_kyc_auth(registNo, custNo)
     update_batch_log()
-    loan_no=apply_loan(custNo,headt)
+    loan_no = apply_loan(custNo, headt)
     if loan_no is None:
         DataBase(which_db).closeDB()
     else:
-        bank_auth(custNo,headt)
+        bank_auth(custNo, headt)
         update_appr_user_stat()
         DataBase(which_db).call_4_proc()
         approve(loan_no)
-        first_apply_sheipihou(loan_no,registNo,custNo,headt)
+        first_apply_sheipihou(loan_no, registNo, custNo, headt)
 
-def first_apply_sheipihou(loan_no,registNo,custNo,headt):
-    insert_risk(loan_no)#匹配产品
-    #停在【通过】状态，用户待提现
-    w=withdraw(registNo,custNo,loan_no,headt)  #app页面点击提现
-    if w==1:
+
+def first_apply_sheipihou(loan_no, registNo, custNo, headt):
+    insert_risk(loan_no)  # 匹配产品
+    # 停在【通过】状态，用户待提现
+    w = withdraw(registNo, custNo, loan_no, headt)  # app页面点击提现
+    if w == 1:
         gaishu(loan_no)
     else:
         pass
     DataBase(which_db).closeDB()
+
+
 def auto_test():
-    registNo=str(random.randint(8000000000,9999999999)) #10位随机数作为手机号
+    registNo = str(random.randint(8000000000, 9999999999))  # 10位随机数作为手机号
     first_apply(registNo)
-#指定手机号，跑后续流程
+
+
+# 指定手机号，跑后续流程
 def bu_ding(registNo):
     update_pwd(registNo)
-    token=login_pwd(registNo)
-    headt=head_token(token)
-    sql="select CUST_NO from cu_cust_reg_dtl where REGIST_NO='"+registNo+"';"
-    custNo=DataBase(which_db).get_one(sql)
-    custNo=custNo[0]
-    loan_no="L2012203278196605433373458432"
+    token = login_pwd(registNo)
+    headt = head_token(token)
+    sql = "select CUST_NO from cu_cust_reg_dtl where REGIST_NO='" + registNo + "';"
+    custNo = DataBase(which_db).get_one(sql)
+    custNo = custNo[0]
+    loan_no = "L2012203278196605433373458432"
     if loan_no is None:
         DataBase(which_db).closeDB()
     else:
-        bank_auth(custNo,headt)
+        bank_auth(custNo, headt)
         # update_appr_user_stat()
         # DataBase(which_db).call_4_proc()
         # approve(loan_no)  #分配审批人员并审批通过
         insert_risk(loan_no)
-        w=withdraw(registNo,custNo,loan_no,headt)
-        if w==1:
+        w = withdraw(registNo, custNo, loan_no, headt)
+        if w == 1:
             gaishu(loan_no)
         else:
             pass
         DataBase(which_db).closeDB()
+
 
 if __name__ == '__main__':
     for i in range(1):
---
+++ public/demo3.py
@@ -0,0 +1,40 @@
+# str='abcabcbb'
+# same=''
+# diff=''
+# for i in range(len(str)):
+#     if str.count(str[i]) > 1:
+#         same+=str[i]
+#     else:
+#         diff+=str[i]
+# # print('重复的元素有：%s'%same)
+# # print('不重复的元素有：%s'%diff)
+#
+# class Solution:
+#    def solve(self, s):
+#       def dp(i, j):
+#          if i >= j:
+#             return 0
+#          if s[i] == s[j]:
+#             return dp(i + 1, j - 1)
+#          else:
+#             return min(dp(i + 1, j), dp(i, j - 1)) + 1
+#       return dp(0, len(s) - 1)
+# ob = Solution()
+# s = "google"
+#print('需要删减的最少字符个数=',ob.solve(s))
+
+
+def lengthOfLongestSubstring(s):
+    d = {}
+    start = 0
+    ans = 0
+    for i, c in enumerate(s):  # 取字符串中第i个字符为c
+        if c in d:  # 如果c在字典里
+            start = max(start, d[c] + 1)  # 将开始位置重新定位
+        d[c] = i  # 记录 c 在字符串的最新位置
+        ans = max(ans, i - start + 1)  # 记录最大不重复值
+    return ans
+
+
+n = lengthOfLongestSubstring('abcabcbb')
+print("不重复最长字符串长度=",n)

3c2fdbd68afd6f2c2a0c20570de0b8606b365b2a
wangshuang <370558913@qq.com>
2022-06-02 14:11:29 +0800

    0602继续优化3，增加说明文档

--- data/var_mex_lp_danqi.py
+++ data/var_mex_lp_danqi.py
@@ -1,7 +1,5 @@
 # __all__ = ['host_api', 'host_action', 'host_mgt', 'head_api', 'host_pay', 'host_coll', 'head_mgt', 'head_pay',
 #            'CONFIGS', 'prodNo', 'which_db', 'appNo', 'shenpiren', 'app_type']
-import time,random,string
-from common.calculate import *
 
 appNo = '201'  # 当前产品号，测试201用多期，马甲包只支持单期无积分和优惠券
 shenpiren = {  # GAID                           app版本号
